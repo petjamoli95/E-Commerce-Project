ce65c04bba440c667f80f6de5540fbbe16aa943f
Pete <petjamoli95@outlook.com>
2024-10-30 16:39:22 +0000

    first commit

---
+++ MVCProject/Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        List<Category> objCategoryList = _db.Categories.ToList();
+        return View(objCategoryList);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Category obj)
+    {
+        if(ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if(id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        Category? categoryFromDb = _db.Categories.Find(id);
+
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(categoryFromDb);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category obj)
+    {
+        if(ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+}
---
+++ MVCProject/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using System.ComponentModel.DataAnnotations;
+global using Microsoft.EntityFrameworkCore;
+global using MVCProject.Models;
+global using MVCProject.Data;
