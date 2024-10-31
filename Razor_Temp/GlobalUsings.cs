03b34b0c19f3f43e21e146799b271e157f741c3e
petjamoli95 <petjamoli95@outlook.com>
2024-10-31 15:56:07 +0000

    Razor Pages

---
+++ Razor_Temp/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using System.ComponentModel.DataAnnotations;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Razor_Temp.Models;
+global using Razor_Temp.Data;
---
+++ Razor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,27 @@
+namespace Razor_Temp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToPage("Index");
+        }
+    }
+}
