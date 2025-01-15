e083e6f72d1d4feda08dd8d4e4ac2236d72c0f17
petjamoli95 <petjamoli95@outlook.com>
2025-01-15 14:16:40 +0000

    Abstracted Product Create and Edit into Upsert

--- MVCProject/Areas/Admin/Controllers/ProductController.cs
+++ MVCProject/Areas/Admin/Controllers/ProductController.cs
@@ -28,7 +28,7 @@ public class ProductController : Controller
         return View(objProductList);
     }
 
-    public IActionResult Create()
+    public IActionResult Upsert(int? id)
     {
         ProductVM productVM = new()
         {
@@ -40,60 +40,38 @@ public class ProductController : Controller
             Product = new Product()
         };
 
-        return View(productVM);
+        if (id == null || id == 0)
+        {
+            return View(productVM);
+        }
+        else
+        {
+            //update 
+            productVM.Product = _unitOfWork.Product.Get(u=>u.Id==id);
+            return View(productVM);
+        }
     }
 
     [HttpPost]
-    public IActionResult Create(ProductVM obj)
+    public IActionResult Upsert(ProductVM productVM, IFormFile? file)
     {
         if(ModelState.IsValid)
         {
-            _unitOfWork.Product.Add(obj.Product);
+            _unitOfWork.Product.Add(productVM.Product);
             _unitOfWork.Save();
             TempData["success"] = "Product created successfully";
             return RedirectToAction("Index");
         }
         else
         {
-            obj.CategoryList = _unitOfWork.Category.GetAll().Select(u=> new SelectListItem
+            productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u=> new SelectListItem
             {
                 Text = u.Name,
                 Value = u.Id.ToString(),
             });
             
-            return View(obj);
+            return View(productVM);
         }
-    }
-
-    public IActionResult Edit(int? id)
-    {
-        if(id == null || id == 0)
-        {
-            return NotFound();
-        }
-
-        Product? productFromDb = _unitOfWork.Product.Get(u=>u.Id==id);
-
-        if (productFromDb == null)
-        {
-            return NotFound();
-        }
-
-        return View(productFromDb);
-    }
-
-    [HttpPost]
-    public IActionResult Edit(Product obj)
-    {
-        if(ModelState.IsValid)
-        {
-            _unitOfWork.Product.Update(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "Product updated successfully";
-            return RedirectToAction("Index");
-        }
-
-        return View();
     }
 
     public IActionResult Delete(int? id)
